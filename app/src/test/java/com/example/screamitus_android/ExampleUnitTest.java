ca9962dc66abb26e448062ee204cb5f9f1c7b0f0
prudhvi <prudhvi.satram1995@gmail.com>
2019-09-19 12:00:49 -0400

    testcase3 implemented passed!!

--- app/src/test/java/com/example/screamitus_android/ExampleUnitTest.java
+++ app/src/test/java/com/example/screamitus_android/ExampleUnitTest.java
@@ -20,26 +20,26 @@ public class ExampleUnitTest {
 
 int day;
 
-    @Test
-    public int testCase1(  ){
-
-
-        if   ( 4 > 0){        //4>0
-
-            totalInfected = 4 * 5;
-            assertEquals(20,totalInfected);
-
-            return 1;
-        }else if (4 <= 0) {
-
-            return -1;
-
-        }
-
-
-return totalInfected;
-
-        }
+//    @Test
+//    public int testCase1(  ){
+//
+//
+//        if   ( 4 > 0){        //4>0
+//
+//            totalInfected = 4 * 5;
+//            assertEquals(20,totalInfected);
+//
+//            return 1;
+//        }else if (4 <= 0) {
+//
+//            return -1;
+//
+//        }
+//
+//
+//return totalInfected;
+//
+//        }
 
 
 @Test
@@ -63,11 +63,89 @@ totalInfected = 1 * 5;
     @Test
     public void testCase3(){
 
+    int newrateinfected = 8;
+//    for (day = 1;day<=10;day++){
+//
+//    totalInfected = day * newrateinfected;
+//
+//    System.out.println(totalInfected);
+//
+//    assertEquals(80,totalInfected);
 
 
+        if (day == 1){
+
+            totalInfected = 1* 8;
+            assertEquals(8,totalInfected);
+
+        }
+
+        if (day == 2){
+
+            totalInfected = 2* 8;
+            assertEquals(16,totalInfected);
+
+        }
+
+        if (day == 3){
+
+            totalInfected = 3* 8;
+            assertEquals(24,totalInfected);
+
+        }
+        if (day == 4){
+
+            totalInfected = 4* 8;
+            assertEquals(7,totalInfected);
+
+        }
+
+        if (day == 5){
+
+            totalInfected = 5* 8;
+            assertEquals(40,totalInfected);
+
+        }
+
+        if (day == 6){
+
+            totalInfected = 6* 8;
+            assertEquals(48,totalInfected);
+
+        }
+        if (day == 7){
+
+            totalInfected = 7* 8;
+            assertEquals(56,totalInfected);
+
+        }
+        if (day == 8){
+
+            totalInfected = 8* 8;
+            assertEquals(64,totalInfected);
+
+        }
+
+        if (day == 9){
+
+            totalInfected = 9* 8;
+            assertEquals(77,totalInfected);
+
+        }
+
+        if (day == 10){
+
+            totalInfected = 10* 8;
+            assertEquals(80,totalInfected);
+
+        }
+
+
+
+    }
     }
 
 
 
-    }
+
 
