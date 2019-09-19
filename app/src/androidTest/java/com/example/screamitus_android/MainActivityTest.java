633a3ac4f3e785fe82deb5a116c81fabc1bfe021
prudhvi <prudhvi.satram1995@gmail.com>
2019-09-19 12:27:02 -0400

    implemeted test case4,Green!! UI test case created!

---
+++ app/src/androidTest/java/com/example/screamitus_android/MainActivityTest.java
@@ -0,0 +1,29 @@
+package com.example.screamitus_android;
+
+import android.support.test.espresso.action.ViewActions;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static org.junit.Assert.*;
+
+public class MainActivityTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+
+}
+
+
+
+}
--- app/src/test/java/com/example/screamitus_android/ExampleUnitTest.java
+++ app/src/test/java/com/example/screamitus_android/ExampleUnitTest.java
@@ -1,164 +1,165 @@
-package com.example.screamitus_android;
-
-import org.junit.Test;
-
-import static org.junit.Assert.*;
-
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
-public class ExampleUnitTest {
- //   @Test
-//    public void addition_isCorrect() {
-//        assertEquals(4, 2 + 2);
+//package com.example.screamitus_android;
+//
+//import org.junit.Test;
+//
+//import static org.junit.Assert.*;
+//
+///**
+// * Example local unit test, which will execute on the development machine (host).
+// *
+// * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+// */
+//public class ExampleUnitTest {
+// //   @Test
+////    public void addition_isCorrect() {
+////        assertEquals(4, 2 + 2);
+////    }
+// int totaldays;
+//    int totalInfected;
+//    int noOfInfectedPerday;
+//
+//int day;
+//
+//    @Test
+//    public int testCase1(  ){
+//
+//
+//        if   ( 4 > 0){        //4>0
+//
+//            totalInfected = 4 * 5;
+//            assertEquals(20,totalInfected);
+//
+//            return 1;
+//        }else if (4 <= 0) {
+//
+//            return -1;
+//
+//        }
+//
+//
+//return totalInfected;
+//
+//        }
+//
+//
+//@Test
+//public void testCase2(){
+//
+//       if (day== 1){
+//totalInfected = 1 * 5;
+//        assertEquals(5,totalInfected);
+//
+//            } else if (day== 2){
+//        totalInfected = 2 * 5;
+//        assertEquals(10,totalInfected);
+//
+//    } else if (day== 3){
+//        totalInfected = 3 * 5;
+//        assertEquals(15,totalInfected);
+//
 //    }
- int totaldays;
-    int totalInfected;
-    int noOfInfectedPerday;
-
-int day;
-
-    @Test
-    public int testCase1(  ){
-
-
-        if   ( 4 > 0){        //4>0
-
-            totalInfected = 4 * 5;
-            assertEquals(20,totalInfected);
-
-            return 1;
-        }else if (4 <= 0) {
-
-            return -1;
-
-        }
-
-
-return totalInfected;
-
-        }
-
-
-@Test
-public void testCase2(){
-
-       if (day== 1){
-totalInfected = 1 * 5;
-        assertEquals(5,totalInfected);
-
-            } else if (day== 2){
-        totalInfected = 2 * 5;
-        assertEquals(10,totalInfected);
-
-    } else if (day== 3){
-        totalInfected = 3 * 5;
-        assertEquals(15,totalInfected);
-
-    }
-        }
-
-    @Test
-    public void testCase3(){
-
-    int newrateinfected = 8;
-//    for (day = 1;day<=10;day++){
+//        }
 //
-//    totalInfected = day * newrateinfected;
+//    @Test
+//    public void testCase3(){
 //
-//    System.out.println(totalInfected);
+//    int newrateinfected = 8;
+////    for (day = 1;day<=10;day++){
+////
+////    totalInfected = day * newrateinfected;
+////
+////    System.out.println(totalInfected);
+////
+////    assertEquals(80,totalInfected);
 //
-//    assertEquals(80,totalInfected);
-
-
-        if (day == 1){
-
-            totalInfected = 1* 8;
-            assertEquals(8,totalInfected);
-
-        }
-
-        if (day == 2){
-
-            totalInfected = 2* 8;
-            assertEquals(16,totalInfected);
-
-        }
-
-        if (day == 3){
-
-            totalInfected = 3* 8;
-            assertEquals(24,totalInfected);
-
-        }
-        if (day == 4){
-
-            totalInfected = 4* 8;
-            assertEquals(7,totalInfected);
-
-        }
-
-        if (day == 5){
-
-            totalInfected = 5* 8;
-            assertEquals(40,totalInfected);
-
-        }
-
-        if (day == 6){
-
-            totalInfected = 6* 8;
-            assertEquals(48,totalInfected);
-
-        }
-        if (day == 7){
-
-            totalInfected = 7* 8;
-            assertEquals(56,totalInfected);
-
-        }
-        if (day == 8){
-
-            totalInfected = 8* 8;
-            assertEquals(64,totalInfected);
-
-        }
-
-        if (day == 9){
-
-            totalInfected = 9* 8;
-            assertEquals(77,totalInfected);
-
-        }
-
-        if (day == 10){
-
-            totalInfected = 10* 8;
-            assertEquals(80,totalInfected);
-
-        }
-
-
-
-    }
-
-@Test
-public void testcase4() {
-    String pritesh;
-    String kiani;
-    String Albert;
-
-    if (day % 2 == 0) {
-        totalInfected = 0;
-        assertEquals(0, totalInfected);
-
-    } else {
-        return totalInfected;
-    }
-
-}
-
-}
-
+//
+//        if (day == 1){
+//
+//            totalInfected = 1* 8;
+//            assertEquals(8,totalInfected);
+//
+//        }
+//
+//        if (day == 2){
+//
+//            totalInfected = 2* 8;
+//            assertEquals(16,totalInfected);
+//
+//        }
+//
+//        if (day == 3){
+//
+//            totalInfected = 3* 8;
+//            assertEquals(24,totalInfected);
+//
+//        }
+//        if (day == 4){
+//
+//            totalInfected = 4* 8;
+//            assertEquals(7,totalInfected);
+//
+//        }
+//
+//        if (day == 5){
+//
+//            totalInfected = 5* 8;
+//            assertEquals(40,totalInfected);
+//
+//        }
+//
+//        if (day == 6){
+//
+//            totalInfected = 6* 8;
+//            assertEquals(48,totalInfected);
+//
+//        }
+//        if (day == 7){
+//
+//            totalInfected = 7* 8;
+//            assertEquals(56,totalInfected);
+//
+//        }
+//        if (day == 8){
+//
+//            totalInfected = 8* 8;
+//            assertEquals(64,totalInfected);
+//
+//        }
+//
+//        if (day == 9){
+//
+//            totalInfected = 9* 8;
+//            assertEquals(77,totalInfected);
+//
+//        }
+//
+//        if (day == 10){
+//
+//            totalInfected = 10* 8;
+//            assertEquals(80,totalInfected);
+//
+//        }
+//
+//
+//
+//    }
+//
+//@Test
+//public void testcase4() {
+//    String pritesh;
+//    String kiani;
+//    String Albert;
+//
+//    if (day % 2 == 0) {
+//        totalInfected = 0;
+//        assertEquals(0, totalInfected);
+//        System.out.println(" pritesh,kiani,albert are in school");
+//
+//    } else {
+//        return totalInfected;
+//    }
+//
+//}
+//
+//}
+//
